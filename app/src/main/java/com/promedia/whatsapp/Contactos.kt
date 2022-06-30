8baf3726012eae991c4f9ed3547e0e6a081c5787
druscky <albeasv@gmail.com>
2022-06-30 16:22:13 +0200

    Añadido el RecyclerView pero faltan cosas del mismo (NO funciona)

---
+++ app/src/main/java/com/promedia/whatsapp/Contactos.kt
@@ -0,0 +1,9 @@
+package com.promedia.whatsapp
+
+import android.graphics.drawable.Drawable
+
+class Contactos (
+val nombre:String,
+val apellido:String,
+val photo:Int) {
+}
---
+++ app/src/main/java/com/promedia/whatsapp/ContactosProvider.kt
@@ -0,0 +1,102 @@
+package com.promedia.whatsapp
+
+class ContactosProvider {
+    companion object {
+        val contactosList = listOf<Contactos>(
+            Contactos(
+                "Lola",
+                "Mento",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Omar",
+                "Ciano",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Omar",
+                "Garita",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Alex",
+                "Plosivo",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Elena",
+                "Nito",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Elsa",
+                "Pito",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Cindy",
+                "Versión",
+                R.drawable.ic_camara
+            ),
+            Contactos(
+                "Ana",
+                "Tomía",
+                R.drawable.ic_camara
+            )
+        )
+    }
+}
+//class SuperHeroProvider {
+//    companion object {
+//        val superHeroList = listOf<SuperHero>(
+//            SuperHero(
+//                "SpiderMan",
+//                "Marvel",
+//                "Peter Parker",
+//                "https://loremflickr.com/200/200/spiderman"
+//            ),
+//            SuperHero(
+//                "Hulk",
+//                "Marvel",
+//                "Bruce Banner",
+//                "https://loremflickr.com/200/200/hulk"
+//            ),
+//            SuperHero(
+//                "Batman",
+//                "DC",
+//                "Bruce Wayne",
+//                "https://loremflickr.com/200/200/batman"
+//            ),
+//            SuperHero(
+//                "Flash",
+//                "DC",
+//                "Jay Garrick",
+//                "https://loremflickr.com/200/200/flash"
+//            ),
+//            SuperHero(
+//                "Deadpool",
+//                "Marvel",
+//                "Wade Winston Wilson",
+//                "https://loremflickr.com/200/200/deadpool"
+//            ),
+//            SuperHero(
+//            "Daredevil",
+//            "Marvel",
+//            "Matt Murdock",
+//            "https://loremflickr.com/200/200/daredevil"
+//            ),
+//            SuperHero(
+//                "Wolverine",
+//                "Marvel",
+//                "James Howlett",
+//                "https://loremflickr.com/200/200/wolverine"
+//            ),
+//            SuperHero(
+//                "Thor",
+//                "Marvel",
+//                "Thor Odinson",
+//                "https://loremflickr.com/200/200/thor"
+//            )
+//        )
+//    }
+//}
